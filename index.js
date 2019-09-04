4d3908546082f7d01af9be4c9eef1914f9f05c7d
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-09-04 00:12:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeliLine = [];
+function takeANumber (katzDeliLine, name) {
+katzDeliLine.push(name)
+return "Welcome, " + name + ". You are number " + katzDeliLine.length + "in line."
+}
+function nowServing(x) {
+  if (x.length ===o) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = x[0];
+    x.splice (0,1);
+    return "Currently serving " + name + ".";
+  }
+}
