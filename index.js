e44a2dcae3840c7f2192e1a78fe854cad9438e88
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-09-04 01:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var katzDeliLine = [];
 function takeANumber (katzDeliLine, name) {
 katzDeliLine.push(name)
 return "Welcome, " + name + ". You are number " + katzDeliLine.length + "in line."
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 }
 function nowServing(x) {
   if (x.length ===o) {
